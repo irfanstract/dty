4268c5f44ead3ed7434dc41c15ed79805a32e27a
me <meh@meh>
2023-08-11 12:40:40 +0700

    ["project" `compiler`] substitute `FatalError` def with `export` `dotty.conception.eutil.{FatalErrorException as FatalError }`

--- compiler/src/dotty/tools/FatalError.scala
+++ compiler/src/dotty/tools/FatalError.scala
@@ -1,3 +1,4 @@
 package dotty.tools
 
-case class FatalError(msg: String) extends Exception(msg)
+// case class FatalError(msg: String) extends Exception(msg)
+export dotty.conception.eutil.{FatalErrorException as FatalError }
